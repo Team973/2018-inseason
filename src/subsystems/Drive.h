3e1c4d5e0090b5f5cd52b1ee83ec54e61c2c773f
CptSa1ty <christophermac112@gmail.com>
2018-01-13 11:55:52 -0800

    Adds gyro to constructor

--- src/subsystems/Drive.h
+++ src/subsystems/Drive.h
@@ -1,12 +1,15 @@
 #include <TalonSRX.h>
 #include <VictorSPX.h>
-using namespace ctre::phoenix::motorcontrol::can
+#include <ADXRS450_Gyro.h>
+using namespace ctre::phoenix::motorcontrol::can;
+using namespace frc;
 
 class Drive {
     private:
-        *TalonSRX m_leftTalon, m_rightTalon;
-        *VictorSPX m_slaveLeftone, m_slaveLefttwo, m_slaveRightone, m_slaveRighttwo;
+        TalonSRX *m_leftTalon, *m_rightTalon;
+        VictorSPX *m_slaveLeftone, *m_slaveLefttwo, *m_slaveRightone, *m_slaveRighttwo;
+        ADXRS450_Gyro *m_driveGyro;
     public:
-         Drive(*TalonSRX leftTalon, *TalonSRX rightTalon, *VictorSPX slaveLeftone,
-               *VictorSPX slaveLefttwo, *VictorSPX slaveRightone, *VictorSPX slaveRighttwo);
+         Drive(TalonSRX *leftTalon, TalonSRX *rightTalon, VictorSPX *slaveLeftone,
+               VictorSPX *slaveLefttwo, VictorSPX *slaveRightone, VictorSPX *slaveRighttwo, ADXRS450_Gyro *driveGyro);
 
