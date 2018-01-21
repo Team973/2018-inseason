736a56d13a71b8dd641883ed0277707554c87bda
Chris <Chris2fourlaw@users.noreply.github.com>
2018-01-21 15:05:53 -0800

    Updates Pin IDs in RobotInfo.h (#24)
    Matches the wiring spreadsheet, everyone should pull to their branch before testing on robot.

--- src/info/RobotInfo.h
+++ src/info/RobotInfo.h
@@ -6,27 +6,33 @@
 #pragma once
 
 using namespace frc;
-namespace frc973 {
-// Talons
-static const int LEFT_DRIVE_A_CAN_ID = 1;
-static const int ELEVATOR_CAN_ID = 60;
-static const int CLAW_LEFT_ROLLER_CAN_ID = 5;
-static const int CLAW_RIGHT_ROLLER_CAN_ID = 6;
-static const int RIGHT_DRIVE_A_CAN_ID = 14;
 
-// Victors
+namespace frc973 {
+// Drive
+static const int LEFT_DRIVE_A_CAN_ID = 1;
 static const int LEFT_DRIVE_B_VICTOR_ID = 2;
 static const int LEFT_DRIVE_C_VICTOR_ID = 3;
-
+static const int RIGHT_DRIVE_A_CAN_ID = 16;
 static const int RIGHT_DRIVE_B_VICTOR_ID = 15;
-static const int RIGHT_DRIVE_C_VICTOR_ID = 16;
+static const int RIGHT_DRIVE_C_VICTOR_ID = 14;
+
+// Elevator
+static const int ELEVATOR_CAN_ID = 4;
+
+// Claw
+static const int CLAW_LEFT_ROLLER_CAN_ID = 5;
+static const int CLAW_RIGHT_ROLLER_CAN_ID = 12;
+static const int CUBE_BANNER_SENSOR_DIN = 0;
+
+// Hanger
+static const int FORKLIFT_TALON_CAN_ID = 6;
+static const int FORKLIFT_VICTOR_ID = 11;
 
 // Solenoids
+static const int HANGER_PTO_PCM_ID = 1;
 
 // Analog
-
-// Digital
-static const int CUBE_BANNER_SENSOR_DIN = 0;
+static const int PRESSURE_ANLG_ID = 0;
 
 // Relays
 static const int COMPRESSOR_RELAY = 0;
