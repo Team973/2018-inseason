56056889e4329c84c3ac0bc776441aaa6bd8af0c
Andrew Nelson <andrew2nelson@gmail.com>
2018-02-01 23:54:41 -0800

    GreyTalonSRX inherits virtually from TalonSRX and from BaseMotorcontroller

--- lib/helpers/GreyTalon.h
+++ lib/helpers/GreyTalon.h
@@ -2,37 +2,55 @@
 #include "Phoenix.h"
 
 namespace frc973 {
-TalonSRX* FactoryReset(TalonSRX* motor) {
-    motor->ConfigSelectedFeedbackSensor(
-        FeedbackDevice::QuadEncoder, 0,
-        10);  // 0 = Not cascaded PID Loop; 10 = in constructor, not in a loop
-    motor->SetSensorPhase(false);
-    motor->SetInverted(false);
-    motor->SetNeutralMode(NeutralMode::Coast);
 
-    motor->ConfigNominalOutputForward(0.0, 10);
-    motor->ConfigNominalOutputReverse(0.0, 10);
-    motor->ConfigPeakOutputForward(1.0, 10);
-    motor->ConfigPeakOutputReverse(-1.0, 10);
+class GreyTalonSRX :
+    public virtual BaseMotorController,
+    public virtual TalonSRX {
 
-    // Gains
-    motor->Config_kP(0, 0.0, 10);
-    motor->Config_kI(0, 0.0, 10);
-    motor->Config_kD(0, 0.0, 10);
-    motor->Config_kF(0, 0.0, 10);
-    motor->ConfigMotionCruiseVelocity(0.0, 10);
-    motor->ConfigMotionAcceleration(0.0, 10);
+public:
+    GreyTalonSRX(int canId)
+        : BaseMotorController(canId | 0x02040000)
+        , TalonSRX(canId)
+    {
+        FactoryReset(this);
+    }
 
-    // Limiting
-    motor->EnableCurrentLimit(false);
-    motor->ConfigPeakCurrentDuration(0, 10);
-    motor->ConfigPeakCurrentLimit(0, 10);
-    motor->ConfigContinuousCurrentLimit(0, 10);
-    motor->EnableVoltageCompensation(false);
-    motor->ConfigVoltageCompSaturation(12, 10);
+    virtual ~GreyTalonSRX() {}
 
-    motor->Set(ControlMode::PercentOutput, 0.0);
+    TalonSRX* FactoryReset(TalonSRX* motor) {
+        motor->ConfigSelectedFeedbackSensor(
+            FeedbackDevice::QuadEncoder, 0,
+            10);  // 0 = Not cascaded PID Loop; 10 = in constructor, not in a loop
+        motor->SetSensorPhase(false);
+        motor->SetInverted(false);
+        motor->SetNeutralMode(NeutralMode::Coast);
 
-    return motor;
-}
+        motor->ConfigNominalOutputForward(0.0, 10);
+        motor->ConfigNominalOutputReverse(0.0, 10);
+        motor->ConfigPeakOutputForward(1.0, 10);
+        motor->ConfigPeakOutputReverse(-1.0, 10);
+
+        // Gains
+        motor->Config_kP(0, 0.0, 10);
+        motor->Config_kI(0, 0.0, 10);
+        motor->Config_kD(0, 0.0, 10);
+        motor->Config_kF(0, 0.0, 10);
+        motor->ConfigMotionCruiseVelocity(0.0, 10);
+        motor->ConfigMotionAcceleration(0.0, 10);
+
+        // Limiting
+        motor->EnableCurrentLimit(false);
+        motor->ConfigPeakCurrentDuration(0, 10);
+        motor->ConfigPeakCurrentLimit(0, 10);
+        motor->ConfigContinuousCurrentLimit(0, 10);
+        motor->EnableVoltageCompensation(false);
+        motor->ConfigVoltageCompSaturation(12, 10);
+
+        motor->Set(ControlMode::PercentOutput, 0.0);
+
+        return motor;
+    }
+
+};
+
 }
