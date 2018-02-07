fb57e1c01520d0b9fcc64e57cc24e8e3458c5252
KyleD973 <jacks8211@gmail.com>
2018-02-06 17:49:16 -0800

    Fixed keyerror problem with format call

--- tools/pathtool/generatePath.py
+++ tools/pathtool/generatePath.py
@@ -10,7 +10,10 @@ Waypoint, Segment, generate_trajectory, generate_tank_trajectory = set_cdll_path
 def openPointStructs(source):
     with open(source) as inputFile:
         sourceJSON = json.load(inputFile)
-        left, right = generate_tank_trajectory(sourceJSON["waypoints"], sourceJSON["timestep"],
+        waypoints = []
+        for waypoint in sourceJSON["waypoints"]:
+            waypoints.append(Waypoint(x = waypoint["x"], y = waypoint["y"], angle = waypoint["angle"]))
+        left, right = generate_tank_trajectory(waypoints, sourceJSON["timestep"],
                 sourceJSON["max_vel"], sourceJSON["max_accel"],
                 sourceJSON["max_jerk"], 16)
         return left, right, sourceJSON
@@ -20,20 +23,21 @@ def writeHeader(target):
     leftTraj, rightTraj, sourceJSON = openPointStructs(jsonPath)
     header.write('''#include "path_to_pathfinder_struct_definitions.h"
     namespace generated_profiles {
-    TrajectoryDescription ''' + sourceJSON[name])
-    header.write("""{
-    .timestep = {0}
-    , .max_vel = {1}
-    , .max_accel = {2}
-    , .max_jerk =   {3}
-    , .wheelbase_width = {4}
-    , .length = {5}
-    , .left_trajectory = { {6}
-    , .right_trajectory = { {7}
-    };
-    }""".format(sourceJSON["timestep"], sourceJSON["max_vel"], sourceJSON["waypoints"],
-                sourceJSON["max_jerk"], sourceJSON["wheelbase_width"], len(leftTraj),
-                leftTraj, rightTraj))
+    TrajectoryDescription ''' + sourceJSON["name"])
+    struct = """{{
+    .timestep = {}
+    , .max_vel = {}
+    , .max_accel = {}
+    , .max_jerk =   {}
+    , .wheelbase_width = {}
+    , .length = {}
+    , .left_trajectory = {{ {} }}
+    , .right_trajectory = {{ {} }}
+    }};
+    }}""".format(sourceJSON["timestep"], sourceJSON["max_vel"], sourceJSON["max_accel"],
+                sourceJSON["max_jerk"], sourceJSON["wheelbase_width"],
+                len(leftTraj), leftTraj, rightTraj)
+    header.write(struct)
     header.close()
 
 writeHeader(headerPath)
