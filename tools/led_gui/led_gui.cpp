27d591b3ca1700ac13b88d5a72f97e249e67a2df
Cole Brinsfield <brinsfield.cole@gmail.com>
2018-02-10 15:16:57 -0800

    Clean up LED Simulator

--- tools/led_gui/led_gui.cpp
+++ tools/led_gui/led_gui.cpp
@@ -8,38 +8,25 @@
 #include <time.h>
 #include <sstream>
 
-
+const int LEDRadius = 15;
 
 sf::CircleShape* createCircles(int num=224) {
-	int radius = 15;
-	int xpos = 0;
-	int ypos = 0;
-	int scalefactor = radius*2;
-
-	sf::CircleShape* v = new sf::CircleShape[num];
+	int diameter = LEDRadius*2;
+	sf::CircleShape* LEDs = new sf::CircleShape[num];
 	for (int i = 0; i < num; i++) {
-		v[i] = sf::CircleShape(radius);
-		v[i].setFillColor(sf::Color::White);
-
-
-		xpos=i;
-		//pick our ypos
-		ypos=0;
-
-		v[i].setPosition(xpos*scalefactor, ypos*scalefactor);
+		LEDs[i] = sf::CircleShape(LEDRadius);
+		LEDs[i].setFillColor(sf::Color::White);
+		LEDs[i].setPosition(i*diameter, 0);
 	}
-	return v;
+	return LEDs;
 }
 
 void updateAndDraw(std::vector<Color> v, sf::RenderWindow& window, sf::CircleShape* disp, unsigned int num=224) {
 	window.clear(sf::Color(200,200,200));
-
-	//update the colors
 	for (unsigned int i = 0; i < num; i++) {
 		disp[i].setFillColor(sf::Color(v.at(i).r, v.at(i).g, v.at(i).b, 255));
 		window.draw(disp[i]);
 	}
-
 	window.display();
 }
 
@@ -51,31 +38,25 @@ int main(int argc, char *argv[]) {
 	}
 	std::istringstream ss(argv[1]);
 	int numLEDs;
-	if (!(ss >> numLEDs))
-		std::cerr << "Invalid number " << argv[1] << '\n';
+	if (!(ss >> numLEDs)) std::cerr << "Invalid number " << argv[1] << '\n';
 	GreyLight* led = new GreyLight(numLEDs);
-
 	struct timespec tim, tim2;
 	tim.tv_sec = 0;
 	tim.tv_nsec = 50*1000000; //the first number represents ms
+  sf::RenderWindow window(sf::VideoMode(numLEDs*LEDRadius*2,LEDRadius*2), "LED Simulator"); //Window with label 'LED Simulator', x=numLed*diameter, y=diameter
+  sf::CircleShape* LEDs = createCircles(numLEDs);
+  while (window.isOpen())
+  {
+      sf::Event event;
+      while (window.pollEvent(event))
+      {
+          if (event.type == sf::Event::Closed)
+              window.close();
+      }
+      led->loop();
+      updateAndDraw(led->state.pixels, window, LEDs, numLEDs);
+      nanosleep(&tim,&tim2);
+  }
 
-    sf::RenderWindow window(sf::VideoMode(numLEDs*15*2,15*2), "LED Simulator");
-    sf::CircleShape* v = createCircles(numLEDs);
-
-
-    led->loop();
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        led->loop();
-        updateAndDraw(led->state.pixels, window, v, numLEDs);
-        nanosleep(&tim,&tim2);
-    }
-
-    return 0;
+  return 0;
 }
