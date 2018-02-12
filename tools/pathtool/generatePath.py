f316e3776c083601e9cd0c4ffee5e62842c64371
KyleD973 <jacks8211@gmail.com>
2018-02-12 10:52:28 -0800

    Fixed header script to build trajectory header

--- tools/pathtool/generatePath.py
+++ tools/pathtool/generatePath.py
@@ -20,6 +20,7 @@ def openPointStructs(source):
 
 def writeHeader(target):
     header = open(target, "w+")
+    countLeft = countRight = 1
     leftTraj, rightTraj, sourceJSON = openPointStructs(jsonPath)
     header.write('''#include "lib/trajectories/structs.h"
 
@@ -27,32 +28,38 @@ using namespace trajectories;
 
 namespace generated_profiles {
 
-    TrajectoryDescription ''' + sourceJSON["name"])
-    header.write('''{{
-    .timestep = {}
-    , .max_vel = {}
-    , .max_accel = {}
-    , .max_jerk =   {}
-    , .wheelbase_width = {}
-    , .length = {}
-    , .left_trajectory = {{\n'''.format(sourceJSON["timestep"], sourceJSON["max_vel"], sourceJSON["max_accel"],
-                sourceJSON["max_jerk"], sourceJSON["wheelbase_width"],
-                len(leftTraj)))
+Segment leftTrajectory[] = {\n''')
     for segment in leftTraj:
         header.write("        {.dt=" + str(segment.dt) + ", .x=" + str(segment.x) + ", .y=" + str(segment.y)
                      + ", .position=" + str(segment.position) + ", .velocity=" + str(segment.velocity)
                      + ", .acceleration=" + str(segment.acceleration) + ", .jerk=" + str(segment.jerk)
-                     + ", .heading=" + str(segment.heading) + "},\n")
-    header.write('''    }
-    , .right_trajectory = {\n''')
+                     + ", .heading=" + str(segment.heading) + "}")
+        if countLeft < len(leftTraj):
+            header.write(",\n")
+        countLeft += 1
+    header.write('''\n};
+Segment rightTrajectory[] = {\n''')
     for segment in rightTraj:
         header.write("        {.dt=" + str(segment.dt) + ", .x=" + str(segment.x) + ", .y=" + str(segment.y)
                      + ", .position=" + str(segment.position) + ", .velocity=" + str(segment.velocity)
                      + ", .acceleration=" + str(segment.acceleration) + ", .jerk=" + str(segment.jerk)
-                     + ", .heading=" + str(segment.heading) + "},\n")
-    header.write('''    }
-    };
-    }''')
+                     + ", .heading=" + str(segment.heading) + "}")
+        if countRight < len(rightTraj):
+            header.write(",\n")
+        countRight += 1
+    header.write('''\n}};
+    TrajectoryDescription {} {{
+        .timestep = {}
+        , .max_vel = {}
+        , .max_accel = {}
+        , .max_jerk = {}
+        , .wheelbase_width = {}
+        , .length = {}
+        , .left_trajectory = leftTrajectory
+        , .right_trajectory = rightTrajectory
+            }};
+        }}'''.format(sourceJSON["name"], sourceJSON["timestep"], sourceJSON["max_vel"], sourceJSON["max_accel"],
+                sourceJSON["max_jerk"], sourceJSON["wheelbase_width"], len(leftTraj)))
     header.close()
 
 writeHeader(headerPath)
