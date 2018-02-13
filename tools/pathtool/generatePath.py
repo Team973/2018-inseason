948c9440ac50775d9a586a1541a978022d68c592
KyleD973 <jacks8211@gmail.com>
2018-02-12 20:59:11 -0800

    Cleaned up generation script

--- tools/pathtool/generatePath.py
+++ tools/pathtool/generatePath.py
@@ -20,7 +20,6 @@ def openPointStructs(source):
 
 def writeHeader(target):
     header = open(target, "w+")
-    countLeft = countRight = 1
     leftTraj, rightTraj, sourceJSON = openPointStructs(jsonPath)
     header.write('''#include "lib/trajectories/structs.h"
 
@@ -29,37 +28,31 @@ using namespace trajectories;
 namespace generated_profiles {
 
 Segment leftTrajectory[] = {\n''')
-    for segment in leftTraj:
-        header.write("        {.dt=" + str(segment.dt) + ", .x=" + str(segment.x) + ", .y=" + str(segment.y)
-                     + ", .position=" + str(segment.position) + ", .velocity=" + str(segment.velocity)
-                     + ", .acceleration=" + str(segment.acceleration) + ", .jerk=" + str(segment.jerk)
-                     + ", .heading=" + str(segment.heading) + "}")
-        if countLeft < len(leftTraj):
-            header.write(",\n")
-        countLeft += 1
-    header.write('''\n};
-Segment rightTrajectory[] = {\n''')
-    for segment in rightTraj:
-        header.write("        {.dt=" + str(segment.dt) + ", .x=" + str(segment.x) + ", .y=" + str(segment.y)
-                     + ", .position=" + str(segment.position) + ", .velocity=" + str(segment.velocity)
-                     + ", .acceleration=" + str(segment.acceleration) + ", .jerk=" + str(segment.jerk)
-                     + ", .heading=" + str(segment.heading) + "}")
-        if countRight < len(rightTraj):
-            header.write(",\n")
-        countRight += 1
-    header.write('''\n}};
-    TrajectoryDescription {} {{
-        .timestep = {}
-        , .max_vel = {}
-        , .max_accel = {}
-        , .max_jerk = {}
-        , .wheelbase_width = {}
-        , .length = {}
-        , .left_trajectory = leftTrajectory
-        , .right_trajectory = rightTrajectory
+    printSegments(leftTraj, header)
+    header.write("Segment rightTrajectory[] = {\n")
+    printSegments(rightTraj, header)
+    header.write('''TrajectoryDescription {} {{
+        .timestep = {},
+        .max_vel = {},
+        .max_accel = {},
+        .max_jerk = {},
+        .wheelbase_width = {},
+        .length = {},
+        .left_trajectory = leftTrajectory,
+        .right_trajectory = rightTrajectory
             }};
         }}'''.format(sourceJSON["name"], sourceJSON["timestep"], sourceJSON["max_vel"], sourceJSON["max_accel"],
                 sourceJSON["max_jerk"], sourceJSON["wheelbase_width"], len(leftTraj)))
     header.close()
+
+def printSegments(trajectory, file):
+    for count, segment in enumerate(trajectory):
+        file.write("        {.dt=" + str(segment.dt) + ", .x=" + str(segment.x) + ", .y=" + str(segment.y)
+                     + ", .position=" + str(segment.position) + ", .velocity=" + str(segment.velocity)
+                     + ", .acceleration=" + str(segment.acceleration) + ", .jerk=" + str(segment.jerk)
+                     + ", .heading=" + str(segment.heading) + "}")
+        if count < len(trajectory) - 1:
+            file.write(",\n")
+    file.write("\n};\n")
 
 writeHeader(headerPath)
