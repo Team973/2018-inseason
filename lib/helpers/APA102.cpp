8d88c4356b9e1b53f53ab068fb6b19db1d76ffa0
Cole Brinsfield <brinsfield.cole@gmail.com>
2018-02-20 17:43:07 -0800

    APA102 updated for style enforcement

--- lib/helpers/APA102.cpp
+++ lib/helpers/APA102.cpp
@@ -7,50 +7,50 @@
  */
 
 #include "APA102.h"
-#include <iostream>
 
-APA102::APA102(int numLEDs, frc::SPI::Port port)
-        : numLEDs(numLEDs), port(port) {
+APA102::APA102(int numLEDs, frc::SPI::Port port) : m_numLEDs(numLEDs) {
     int numBytesForStrip =
-        4 * numLEDs + 4 + 4;  // 4*numLeds+startFrame length + endFrame length
-    endFrameOffset = 4 * numLEDs + 4;  // 4*numLEDs+startFrame length;
-    ledBuffer = std::vector<uint8_t>(numBytesForStrip);
+        4 * m_numLEDs + 4 + 4;  // 4*numLeds+startFrame length + endFrame length
+    m_endFrameOffset = 4 * m_numLEDs + 4;  // 4*numLEDs+startFrame length;
+    m_ledBuffer = std::vector<uint8_t>(numBytesForStrip);
     /*
      * One frame of led data consists of four 0x00 bytes, sequential 0xFF, R, G,
      * B for each LED, and then 4 0xFF bytes
      */
     for (int i = 0; i < 4; i++) {
-        ledBuffer.at(i) = startFrame[i];
+        m_ledBuffer.at(i) = m_startFrame[i];
     }
     for (int i = 0; i < 4; i++) {
-        ledBuffer.at(i + endFrameOffset) = endFrame[i];
+        m_ledBuffer.at(i + m_endFrameOffset) = m_endFrame[i];
     }
-    spi = new frc::SPI(port);
-    spi->SetMSBFirst();
-    spi->SetClockActiveLow();
-    spi->SetClockRate(SPI_CLK_RATE);
-    spi->SetSampleDataOnFalling();
+    m_spi = new frc::SPI(port);
+    m_spi->SetMSBFirst();
+    m_spi->SetClockActiveLow();
+    m_spi->SetClockRate(SPI_CLK_RATE);
+    m_spi->SetSampleDataOnFalling();
 }
 
 void APA102::show(std::vector<Color> pixels) {
     int CHUNK_SIZE = 127;  // max TX size
-    for (int i = 0; i < numLEDs; i++) {
-        ledBuffer.at(i * bytesPerLED + led0Offset + globalOffset) = 0xFF;
-        ledBuffer.at(i * bytesPerLED + led0Offset + redOffset) = pixels.at(i).r;
-        ledBuffer.at(i * bytesPerLED + led0Offset + greenOffset) =
+    for (int i = 0; i < m_numLEDs; i++) {
+        m_ledBuffer.at(i * m_bytesPerLED + m_led0Offset + m_globalOffset) =
+            0xFF;
+        m_ledBuffer.at(i * m_bytesPerLED + m_led0Offset + m_redOffset) =
+            pixels.at(i).r;
+        m_ledBuffer.at(i * m_bytesPerLED + m_led0Offset + m_greenOffset) =
             pixels.at(i).g;
-        ledBuffer.at(i * bytesPerLED + led0Offset + blueOffset) =
+        m_ledBuffer.at(i * m_bytesPerLED + m_led0Offset + m_blueOffset) =
             pixels.at(i).b;
     }
-    int ledBufferLength = ledBuffer.size();
+    int ledBufferLength = m_ledBuffer.size();
     for (int offset = 0; offset < ledBufferLength; offset += CHUNK_SIZE) {
         int startIndex = offset;
         int endIndex = std::min(offset + CHUNK_SIZE, ledBufferLength);
         int size = endIndex - startIndex;
         uint8_t txArray[size];
         for (int i = 0; i < size; i++) {
-            txArray[i] = ledBuffer.at(startIndex + i);
+            txArray[i] = m_ledBuffer.at(startIndex + i);
         }
-        spi->Write(txArray, size);
+        m_spi->Write(txArray, size);
     }
 }
--- lib/helpers/APA102.h
+++ lib/helpers/APA102.h
@@ -15,21 +15,19 @@ public:
     void show(std::vector<Color> pixels);
 
 private:
-    uint8_t startFrame[4] = {0x00, 0x00, 0x00, 0x00};
-    uint8_t endFrame[4] = {0xFF, 0xFF, 0xFF, 0xFF};
-    int numLEDs;
-    const int globalOffset = 0;
-    const int blueOffset = 1;
-    const int redOffset = 3;
-    const int greenOffset = 2;
-    const int bytesPerLED = 4;
-    const int led0Offset = 4;  // length of startFrame
-    int endFrameOffset;        // will be dependent on num Leds
-    frc::SPI::Port port;
-    frc::SPI* spi;
+    uint8_t m_startFrame[4] = {0x00, 0x00, 0x00, 0x00};
+    uint8_t m_endFrame[4] = {0xFF, 0xFF, 0xFF, 0xFF};
+    int m_numLEDs;
+    const int m_globalOffset = 0;
+    const int m_blueOffset = 1;
+    const int m_redOffset = 3;
+    const int m_greenOffset = 2;
+    const int m_bytesPerLED = 4;
+    const int m_led0Offset = 4;  // length of startFrame
+    int m_endFrameOffset;        // will be dependent on num Leds
+    frc::SPI* m_spi;
     const int SPI_CLK_RATE = 13000000;
-    std::vector<uint8_t> ledBuffer;
-    const int ledMaxVal = 255;
+    std::vector<uint8_t> m_ledBuffer;
 };
 
 #endif
