970cca2756d28bbecc0567e8f1e3bde44700b5ef
Chris <chris2fourlaw@gmail.com>
2018-02-24 11:31:41 -0800

    Add air pressure guage, debug tab, and more

--- GreyDash/src/ui.js
+++ GreyDash/src/ui.js
@@ -1,5 +1,6 @@
-const fs = require('fs');
+require('../node_modules/justgage/justgage.js');
 const smoothie = require('smoothie');
+const fs = require('fs');
 
 const config = JSON.parse(fs.readFileSync(`${__dirname}/configlisteners.json`, 'utf-8'));
 
@@ -14,10 +15,16 @@ for (let i = 0; i < config.charts.length; i += 1) {
   if (config.charts[i].settings.maxValue === undefined) {
     config.charts[i].settings.maxValue = 50;
   }
+  if (config.charts[i].settings.debug === undefined) {
+    config.charts[i].settings.debug = false;
+  }
 }
 for (let i = 0; i < config.indicators.length; i += 1) {
   if (config.indicators[i].settings.fixedDecimals === undefined) {
     config.indicators[i].settings.fixedDecimals = true;
+  }
+  if (config.indicators[i].settings.debug === undefined) {
+    config.indicators[i].settings.debug = false;
   }
 }
 
@@ -38,7 +45,9 @@ const ui = {
     autoSelect: document.getElementById('autoSelect'),
   },
   charts: document.getElementById('charts'),
+  debugCharts: document.getElementById('debugCharts'),
   indicators: document.getElementById('indicators'),
+  debugIndicators: document.getElementById('debugIndicators'),
   custom: {
     drive: {
       gyro: {
@@ -48,6 +57,14 @@ const ui = {
         visualVal: 0,
         gyroDial: document.getElementById('gyroDial'),
         gyroNumber: document.getElementById('gyroNumber'),
+      },
+      debugGyro: {
+        container: document.getElementById('debugGyro'),
+        val: 0,
+        offset: 0,
+        visualVal: 0,
+        debugGyroDial: document.getElementById('debugGyroDial'),
+        debugGyroNumber: document.getElementById('debugGyroNumber'),
       },
     },
   },
@@ -93,7 +110,11 @@ for (let i = 0; i < config.charts.length; i += 1) {
   config.charts[i].div.appendChild(config.charts[i].displayTitle);
   config.charts[i].div.appendChild(document.createElement('br'));
   config.charts[i].div.appendChild(config.charts[i].displayChart);
-  ui.charts.appendChild(config.charts[i].div);
+  if (config.charts[i].settings.debug) {
+    ui.debugCharts.appendChild(config.charts[i].div);
+  } else {
+    ui.charts.appendChild(config.charts[i].div);
+  }
 }
 
 /**
@@ -123,6 +144,14 @@ for (let i = 0; i < config.indicators.length; i += 1) {
   config.indicators[i].displayTitle.appendChild(config.indicators[i].displayValue);
   config.indicators[i].div.appendChild(config.indicators[i].displayTitle);
   ui.indicators.appendChild(config.indicators[i].div);
+  if (config.indicators[i].settings.debug) {
+    ui.debugIndicators.appendChild(config.indicators[i].div);
+  } else if (config.indicators[i].settings.bothTabs) {
+    ui.indicators.appendChild(config.indicators[i].div);
+    ui.debugIndicators.appendChild(config.indicators[i].div);
+  } else {
+    ui.indicators.appendChild(config.indicators[i].div);
+  }
 }
 
 /**
@@ -141,7 +170,19 @@ const updateGyro = (key, value) => {
   ui.custom.drive.gyro.gyroDial.style.transform = `rotate(-${ui.custom.drive.gyro.visualVal}deg)`;
   ui.custom.drive.gyro.gyroNumber.innerHTML = `${ui.custom.drive.gyro.visualVal}º`;
 };
+const updateDebugGyro = (key, value) => {
+  ui.custom.drive.debugGyro.val = value;
+  ui.custom.drive.debugGyro.visualVal = Math.floor(ui.custom.drive.debugGyro.val
+                                              - ui.custom.drive.debugGyro.offset);
+  ui.custom.drive.debugGyro.visualVal %= 360;
+  if (ui.custom.drive.debugGyro.visualVal < 0) {
+    ui.custom.drive.debugGyro.visualVal += 360;
+  }
+  ui.custom.drive.debugGyro.debugGyroDial.style.transform = `rotate(-${ui.custom.drive.debugGyro.visualVal}deg)`;
+  ui.custom.drive.debugGyro.debugGyroNumber.innerHTML = `${ui.custom.drive.debugGyro.visualVal}º`;
+};
 NetworkTables.addKeyListener('/SmartDashboard/drive/gyro/angle', updateGyro);
+NetworkTables.addKeyListener('/SmartDashboard/drive/gyro/angle', updateDebugGyro);
 
 NetworkTables.addKeyListener('/robot/time', (key, value) => {
   // This is an example of how a dashboard could display the remaining time in a match.
@@ -155,6 +196,31 @@ ui.custom.drive.gyro.container.addEventListener('click', () => {
   ui.custom.drive.gyro.offset = ui.custom.drive.gyro.val;
   // Trigger the gyro to recalculate value.
   updateGyro('/SmartDashboard/drive/gyro/angle', ui.custom.drive.gyro.val);
+});
+ui.custom.drive.debugGyro.container.addEventListener('click', () => {
+  // Store previous gyro val, will now be subtracted from val for callibration
+  ui.custom.drive.debugGyro.offset = ui.custom.drive.debugGyro.val;
+  // Trigger the gyro to recalculate value.
+  updateDebugGyro('/SmartDashboard/drive/gyro/angle', ui.custom.drive.gyro.val);
+});
+
+// Compressor
+const airGuage = new JustGage({
+  id: 'airPressure',
+  min: 0,
+  max: 60,
+  title: 'Air Pressure',
+});
+const debugAirGuage = new JustGage({
+  id: 'debugAirPressure',
+  min: 0,
+  max: 60,
+  title: 'Air Pressure',
+});
+
+NetworkTables.addKeyListener('/SmartDashboard/misc/compressor/pressure', (key, value) => {
+  airGuage.value = value;
+  debugAirGuage.value = value;
 });
 
 window.addEventListener('error', (ev) => {
