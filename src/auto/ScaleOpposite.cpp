d5b2a513ea3ff8bba200802978b5f04906b3fc45
KyleD973 <jacks8211@gmail.com>
2018-03-04 16:38:21 -0800

    Tuned scale opposite

--- src/auto/ScaleOpposite.cpp
+++ src/auto/ScaleOpposite.cpp
@@ -1,10 +1,14 @@
 #include "src/auto/ScaleOpposite.h"
-#include "src/auto/profiles/rightscaleopposite_trajectory.h"
-#include "src/auto/profiles/leftscaleopposite_trajectory.h"
+#include "src/auto/profiles/rightscaleoppositea_trajectory.h"
+#include "src/auto/profiles/leftscaleoppositea_trajectory.h"
+#include "src/auto/profiles/rightscaleoppositeb_trajectory.h"
+#include "src/auto/profiles/leftscaleoppositeb_trajectory.h"
 
 using namespace frc;
-using namespace left_scale_opposite;
-using namespace right_scale_opposite;
+using namespace left_scale_opposite_a;
+using namespace right_scale_opposite_a;
+using namespace left_scale_opposite_b;
+using namespace right_scale_opposite_b;
 
 namespace frc973 {
 ScaleOpposite::ScaleOpposite(Drive *drive, Elevator *elevator, Intake *intake,
@@ -24,12 +28,13 @@ void ScaleOpposite::Execute(AutoRoutineBase::AutoDirection direction) {
     switch (m_autoState) {
         case 0:
             if (direction == AutoRoutineBase::AutoDirection::Right) {
-                m_drive->SplineDrive(&left_scale_opposite::left_scale_opposite,
-                                     Drive::RelativeTo::Now);
+                m_drive->SplineDrive(
+                    &left_scale_opposite_a::left_scale_opposite_a,
+                    Drive::RelativeTo::Now);
             }
             else if (direction == AutoRoutineBase::AutoDirection::Left) {
                 m_drive->SplineDrive(
-                    &right_scale_opposite::right_scale_opposite,
+                    &right_scale_opposite_a::right_scale_opposite_a,
                     Drive::RelativeTo::Now);
             }
             m_intake->Open();
@@ -40,15 +45,30 @@ void ScaleOpposite::Execute(AutoRoutineBase::AutoDirection direction) {
             m_autoState++;
             break;
         case 1:
-            if (GetMsecTime() - m_autoTimer > 4500) {
+            if (m_drive->GetSplinePercentComplete() > 1.0) {
+                if (direction == AutoRoutineBase::AutoDirection::Right) {
+                    m_drive->SplineDrive(
+                        &left_scale_opposite_b::left_scale_opposite_b,
+                        Drive::RelativeTo::Now);
+                }
+                else if (direction == AutoRoutineBase::AutoDirection::Left) {
+                    m_drive->SplineDrive(
+                        &right_scale_opposite_b::right_scale_opposite_b,
+                        Drive::RelativeTo::Now);
+                }
+                m_autoState++;
+            }
+            break;
+        case 2:
+            if (GetMsecTime() - m_autoTimer > 7000) {
                 m_elevator->SetPosition(Elevator::SCALE_HIGH);
                 m_autoTimer = GetMsecTime();
                 m_autoState++;
             }
             break;
-        case 2:
+        case 3:
             if (m_drive->GetSplinePercentComplete() > 0.95 ||
-                m_drive->OnTarget() || GetMsecTime() - m_autoTimer > 6000) {
+                m_drive->OnTarget()) {
                 m_claw->cubeLaunch();
                 m_autoState++;
             }
