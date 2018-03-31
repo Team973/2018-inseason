86eb4434928d655b7f3fa5cc21af1a055f35ef7b
Andrew Nelson <andrew2nelson@gmail.com>
2018-03-31 08:01:38 -0700

    Reverted elevator feed-forward to comp bot

--- src/info/RobotInfo.h
+++ src/info/RobotInfo.h
@@ -22,7 +22,7 @@ static const int RIGHT_DRIVE_C_VICTOR_ID = 3;
 static const int ELEVATOR_CAN_ID = 4;
 static const double ELEVATOR_INCHES_PER_CLICK = 8.0 / 4096.0;
 static const double ELEVATOR_SOFT_HEIGHT_LIMIT = 80.5;
-static const double ELEVATOR_FEED_FORWARD = 0.15;
+static const double ELEVATOR_FEED_FORWARD = 0.10;
 
 // Solenoids
 static const int PCM_CAN_ID = 17;
