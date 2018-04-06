7e84a297e750ecfa47ecf211bb143d737973af38
Chris <chris2fourlaw@gmail.com>
2018-04-06 14:22:24 -0700

    Better doxygen
    use lib too!

--- tools/doxygen/doxygen.sh
+++ tools/doxygen/doxygen.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sed "s#CHANGE_ME#$1#g" tools/doxygen/Doxyfile.in > tools/doxygen/Doxyfile
 doxygen tools/doxygen/Doxyfile
 case "$(uname -s)" in
     Linux*)     xdg-open documentation/html/index.html;;
