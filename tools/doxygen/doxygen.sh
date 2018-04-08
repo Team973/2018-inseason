9b1432c2855139d5af176c3a8fa54430a3d8836f
Chris <chris2fourlaw@gmail.com>
2018-04-08 13:37:41 -0700

    Generate docs folder and option to generate third_party

--- tools/doxygen/doxygen.sh
+++ tools/doxygen/doxygen.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+if [[ $1 != 'short' ]]; then
+  sed 's/CHANGE_ME/YES/g' tools/doxygen/Doxyfile.in > tools/doxygen/Doxyfile.long
+  sed 's/lib src/lib src third_party/g' tools/doxygen/Doxyfile.long > tools/doxygen/Doxyfile
+else
+  sed 's/CHANGE_ME/NO/g' tools/doxygen/Doxyfile.in > tools/doxygen/Doxyfile
+fi
+
 doxygen tools/doxygen/Doxyfile
+
 case "$(uname -s)" in
     Linux*)     xdg-open documentation/html/index.html;;
     Darwin*)    open documentation/html/index.html;;
