2b75d47f93526bd595303a95605d79063a370ebc
Chris <chris2fourlaw@gmail.com>
2018-05-09 21:46:02 -0700

    src/subsystems

--- src/subsystems/Claw.h
+++ src/subsystems/Claw.h
@@ -12,32 +12,75 @@ namespace frc973 {
 class TaskMgr;
 class LogSpreadsheet;
 
+/**
+ * Claw Subsystem.
+ */
 class Claw : public CoopTask {
 public:
+    /**
+     * Construct a claw.
+     * @param scheduler TaskMgr object.
+     * @param logger LogSpreadsheet object.
+     * @param rightCubeSensor The right cube banner sensor.
+     * @param leftCubeSensor The left cube banner sensor.
+     * @param leftRoller The Talon for left intake.
+     * @param rightRoller The Talon for right intake.
+     * @param cubeClamp The solenoid for cube clamp.
+     * @param cubeSpring The solenoid for the cube spring.
+     */
     Claw(TaskMgr *scheduler, LogSpreadsheet *logger,
          DigitalInput *rightCubeSensor, DigitalInput *leftCubeSensor,
          TalonSRX *leftRoller, TalonSRX *rightRoller, Solenoid *cubeClamp,
          Solenoid *cubeSpring);
     virtual ~Claw();
 
-    /*
-     * When called, opens the claw arms
+    /**
+     * Opens the claw arms
      */
     void OpenClaw();
 
-    /*
-     * When called, closes claw arms
+    /**
+     * Close the claw arms with little pressure.
      */
     void SoftCloseClaw();
+
+    /**
+     * Close the claw arms with more pressure.
+     */
     void HardCloseClaw();
 
+    /**
+     * Runs the intake forward at a given power.
+     * @param power Percentage of power, from -1.0 to 1.0.
+     */
     void RunIntake(double power);
+
+    /**
+     * Runs the intake backwards at a given power.
+     * @param power Percentage of power, from -1.0 to 1.0.
+     */
     void EjectCube(double power);
+
+    /**
+     * Runs the intake forward at -0.15 to hold the cube in.
+     */
     void HoldCube();
+
+    /**
+     * Sets the intake to 0.0.
+     */
     void StopIntake();
 
+    /**
+     * True/false if the cube is in.
+     * @return Is cube in?
+     */
     bool IsCubeIn() const;
 
+    /**
+     * Periodically update information about the claw.
+     * @param mode The current robot mode.
+     */
     void TaskPeriodic(RobotMode mode);
 
 private:
--- src/subsystems/Elevator.h
+++ src/subsystems/Elevator.h
@@ -21,58 +21,83 @@ namespace frc973 {
 class TaskMgr;
 class LogSpreadsheet;
 
+/**
+ * Elevator Subsystem.
+ */
 class Elevator : public CoopTask {
 public:
+    /**
+     * Defines the elevator control states for Talon.
+     */
     enum ElevatorState
     {
-        manualVoltage,
-        motionMagic
+        manualVoltage, /**< Control the motors with manual voltage. */
+        motionMagic /**< Control the motors using position w/ Motion Magic. */
     };
 
-    static constexpr double GROUND = 0.0;
-    static constexpr double VAULT = 4.5;
-    static constexpr double LOW_GOAL = 30.0;
-    static constexpr double HANGING = 55.0;
-    static constexpr double SCALE_LOW = 58.0;
-    static constexpr double SCALE_MID = 70.0;
-    static constexpr double SCALE_HIGH = 79.5;
+    static constexpr double GROUND = 0.0;      /**< Ground preset. */
+    static constexpr double VAULT = 4.5;       /**< Vault preset. */
+    static constexpr double LOW_GOAL = 30.0;   /**< Switch preset. */
+    static constexpr double HANGING = 55.0;    /**< Hang preset. */
+    static constexpr double SCALE_LOW = 58.0;  /**< Lower scale preset. */
+    static constexpr double SCALE_MID = 70.0;  /**< Middle scale preset. */
+    static constexpr double SCALE_HIGH = 79.5; /**< Higher scale preset. */
 
-    static constexpr double ELEVATOR_SOFT_HEIGHT_LIMIT = 80.5;
-    static constexpr double ELEVATOR_INCHES_PER_CLICK = 8.0 / 4096.0;
-    static constexpr double ELEVATOR_FEED_FORWARD = 0.10;
+    static constexpr double ELEVATOR_SOFT_HEIGHT_LIMIT =
+        80.5; /**< Soft elevator height. */
+    static constexpr double ELEVATOR_INCHES_PER_CLICK =
+        8.0 / 4096.0; /**< Encoder in/click */
+    static constexpr double ELEVATOR_FEED_FORWARD =
+        0.10; /**< The elevator's feed forward. */
 
+    /**
+     * Contruct an elevator.
+     * @param scheduler TaskMgr object.
+     * @param logger LogSpreadsheet object.
+     * @param elevatorMotor The elevator Talon.
+     */
     Elevator(TaskMgr *scheduler, LogSpreadsheet *logger,
              TalonSRX *elevatorMotor);
     virtual ~Elevator();
 
     /**
-     * Sets Elevator Position using Motion Magic
-     *
-     * @param position: the position goal
-     **/
+     * Set the elevator position using Motion Magic.
+     * @param position The position goal.
+     */
     void SetPosition(double position);
 
     /**
-     * Sets Elevator Power
-     *
-     * @param power: power being sent to the motor from -1.0 to 1.0
-     **/
+     * Set the elevator power.
+     * @param power The power being sent to the motor from -1.0 to 1.0
+     */
     void SetPower(double power);
 
     /**
-     * @return: returns current elevator position in sensor units
-     **/
+     * Get the current position.
+     * @return The current position in sensor units.
+     */
     float GetPosition() const;
 
+    /**
+     * Zero the current position.
+     */
     void ZeroPosition();
 
+    /**
+     * Enable brake mode on the elevator Talon.
+     */
     void EnableBrakeMode();
+
+    /**
+     * Enable coast mode on the elevator Talon.
+     */
     void EnableCoastMode();
 
     /**
      * Update function synonymous to TeleopContinuous that gets called
-     *continuously
-     **/
+     * continuously.
+     * @param mode The current robot mode.
+     */
     void TaskPeriodic(RobotMode mode);
 
 private:
